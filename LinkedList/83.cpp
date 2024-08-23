56f971815a35935de0dfa88664cd46a82290732f
szktringoml <szktringoml@gmail.com>
2024-08-23 10:50:22 +0900

    step3

--- LinkedList/83.cpp
+++ LinkedList/83.cpp
@@ -12,13 +12,44 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         ListNode *current = head;
-        while(current && current->next){
-            if(current->next && current->val == current->next->val){
+        while(current){
+            while(current->next && current->val == current->next->val){
                 current->next = current->next->next;
-            }else{
-              current = current->next;
             }
+            current = current->next;
         }
         return head;
+    }
+};
+
+//========================================================================
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *dummyHead = new ListNode(-1, head);
+        ListNode *current = dummyHead;
+        while(current->next && current->next->next){
+            if(current->next->val != current->next->next->val){
+                current = current->next;
+                continue;
+            }
+            ListNode *duplicateNode = current->next;
+            while(duplicateNode->next && duplicateNode->val == duplicateNode->next->val){
+                duplicateNode = duplicateNode->next;
+            }
+            current->next = duplicateNode->next;
+        }
+        return dummyHead->next;
     }
 };
